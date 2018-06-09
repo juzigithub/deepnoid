8ba9332c8a9376f07b737ad4de7e036a84613b45
pms <sunkid100000@gmail.com>
2018-06-09 20:22:51 +0900

    z

--- unet/aneurysm_unet/config.py
+++
@@ -1,8 +0,0 @@
-
-
-TRAIN_DATA_PATH = '/home/mspark/project/data/Brain_Aneurysm_new_dataset/test/'
-VAL_DATA_PATH = '/home/mspark/project/data/Brain_Aneurysm_new_dataset/test/'
-PKL_DATA_PATH = '/home/mspark/project/data/Brain_Aneurysm_new_dataset/pkl/'
-REBUILD_PKL = False
-PKL_NAME = 'train.pkl'
-LOG_DATA_PATH = '/home/mspark/project/aneurysm/unet/results/'
