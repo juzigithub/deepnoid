0f60521f06a39b6106ecabbeb0699e9d6b5660d4
pms <sunkid100000@gmail.com>
2018-06-17 20:07:38 +0900

    z

--- unet/aneurysm_unet/merging/model_mobilenet/model.py
+++ unet/aneurysm_unet/merging/model_mobilenet/model.py
@@ -6,6 +6,8 @@ by 신형은 주임
 import tensorflow as tf
 import utils
 import config as cfg
+# import Unet.aneurysm_unet.merging.utils as utils
+# import Unet.aneurysm_unet.merging.config as cfg
 
 
 class Model:
@@ -31,23 +33,18 @@ class Model:
 
         # 라벨이미지 역시 foreground와 background로 분리합니다
         self.foreground_truth, self.background_truth = tf.split(self.labels, [1, 1], 3)
-##################################################
         self.loss_1 = utils.select_loss(mode=cfg.LOSS_FUNC, output=self.foreground_predicted, target=self.foreground_truth)
         self.loss_2 = utils.select_loss(mode=cfg.LOSS_FUNC, output=self.background_predicted, target=self.background_truth)
         self.loss = cfg.LAMBDA * self.loss_1 + (1 - cfg.LAMBDA) * self.loss_2
-##################################################
         self.results = list(utils.iou_coe(output=self.foreground_predicted, target=self.foreground_truth))
 
 
     def mobilenet(self):
         # start down sampling by depth n.
-
-###############################
         self.down_conv = [0] * cfg.DEPTH
         self.down_pool = [0] * cfg.DEPTH
         self.up_conv = [0] * cfg.DEPTH
         self.up_pool = [0] * cfg.DEPTH
-###############################
 
         with tf.variable_scope('down'):
 
@@ -55,30 +52,69 @@ class Model:
             channel_n = cfg.INIT_N_FILTER
             pool_size = cfg.IMG_SIZE
 
-            # 처음 실행하면 모델 구조 나오도록 ?!
-            # for i in range(cfg.DEPTH):
-            #     pool_size //= 2
-            #     inputs = utils.unet_down_block(inputs, self.down_conv, self.down_pool, channel_n, pool_size, cfg.GROUP_N, self.training, i)
-            #     channel_n *= 2
-
+            if cfg.FIRST_DOWNSAMPLING:
+                pool_size //= 2
+                inputs = utils.select_downsampling('first_downsampling', inputs, [], channel_n, pool_size, cfg.DOWNSAMPLING_TYPE)
 
             for i in range(cfg.DEPTH):
                 pool_size //= 2
-                self.down_conv[i] = utils.depthwise_separable_convlayer('dsconv' + str(i), inputs, channel_n, cfg.WIDTH_MULTIPLIER, cfg.GROUP_N, self.training, i)
+                self.down_conv[i] = utils.depthwise_separable_convlayer(name = 'dsconv' + str(i),
+                                                                        inputs = inputs,
+                                                                        channel_n = channel_n,
+                                                                        width_mul = cfg.WIDTH_MULTIPLIER,
+                                                                        group_n = cfg.GROUP_N,
+                                                                        act_fn = cfg.ACTIVATION_FUNC,
+                                                                        norm_type = cfg.NORMALIZATION_TYPE,
+                                                                        training = self.training,
+                                                                        idx = i)
                 print('down_conv', self.down_conv[i])
                 channel_n *= 2
-                self.down_pool[i] = utils.select_downsampling(str(i) + '_downsampling', self.down_conv[i], self.down_pool[i], channel_n, pool_size, cfg.DOWNSAMPLING_TYPE)
+                self.down_pool[i] = utils.select_downsampling(name = str(i) + '_downsampling',
+                                                              down_conv = self.down_conv[i],
+                                                              down_pool = self.down_pool[i],
+                                                              channel_n = channel_n,
+                                                              pool_size = pool_size,
+                                                              mode = cfg.DOWNSAMPLING_TYPE)
+
                 inputs = tf.identity(self.down_pool[i])
                 print('down_pool', inputs)
 
-            inputs = utils.unet_same_block(inputs, channel_n, cfg.GROUP_N, self.training)
+            inputs = utils.unet_same_block(inputs = inputs,
+                                           channel_n = channel_n,
+                                           group_n = cfg.GROUP_N,
+                                           act_fn = cfg.ACTIVATION_FUNC,
+                                           norm_type = cfg.NORMALIZATION_TYPE,
+                                           training = self.training)
 
         with tf.variable_scope('up'):
 
             for i in reversed(range(cfg.DEPTH)):
                 channel_n //= 2
                 pool_size *= 2
-                inputs = utils.unet_up_block(inputs, self.down_conv, self.up_conv, self.up_pool, channel_n, pool_size, cfg.GROUP_N, self.training, i)
+                inputs = utils.select_upsampling(name = str(i) + '_upsampling',
+                                                 up_conv = inputs,
+                                                 up_pool = self.up_pool[i],
+                                                 channel_n = channel_n,
+                                                 pool_size = pool_size,
+                                                 mode = cfg.UPSAMPLING_TYPE)
+                print('up_pool', inputs)
+
+                inputs = utils.unet_up_block(inputs = inputs,
+                                             downconv_list = self.down_conv,
+                                             upconv_list = self.up_conv,
+                                             pool_list = self.up_pool,
+                                             channel_n = channel_n,
+                                             group_n = cfg.GROUP_N,
+                                             act_fn = cfg.ACTIVATION_FUNC,
+                                             norm_type = cfg.NORMALIZATION_TYPE,
+                                             training = self.training,
+                                             idx = i)
+                print('up_conv', inputs)
+
+            if cfg.FIRST_DOWNSAMPLING:
+                channel_n //= 2
+                pool_size *= 2
+                inputs= utils.select_upsampling('last_upsampling', inputs, [], channel_n, pool_size, cfg.UPSAMPLING_TYPE)
 
             up_conv_f = utils.conv2D('final_upconv', inputs, cfg.N_CLASS, [1,1], [1,1], 'SAME')
 
