5769dd491e90d40aac3191e65add3e38112372db
pms <sunkid100000@gmail.com>
2018-07-11 15:16:21 +0900

    z

--- brats2018/train5.py
+++ brats2018/train5.py
@@ -155,50 +155,50 @@ class Train:
                     if save_yn:
                         self._make_path(epoch)
 #########################################################
-                    # for batch in tl.iterate.minibatches(inputs=train_X, targets=train_Y,
-                    #                                     batch_size=cfg.BATCH_SIZE, shuffle=True):
-                    #     batch_x, batch_y = batch
-                    #
-                    #     # make_one_hot
-                    #     key = np.array(cfg.TRAIN_LABEL)
-                    #     # [0,1,2,3]
-                    #     _, index = np.unique(batch_y, return_inverse=True)
-                    #     # 4 -> 3
-                    #     seg = key[index].reshape(batch_y.shape)
-                    #
-                    #     batch_y = np.eye(4)[seg]
-                    #
-                    #
-                    #     # step_time = time.time()
-                    #     tr_feed_dict = {self.model.X: batch_x,
-                    #                     self.model.Y: batch_y,
-                    #                     self.model.training: True,
-                    #                     self.model.drop_rate: 0.2}
-                    #
-                    #     # cost, _ = sess.run([self.model.loss, self.optimizer], feed_dict=tr_feed_dict)
-                    #     cost, _ = sess.run([self.model.loss, self.optimizer], feed_dict=tr_feed_dict)
-                    #
-                    #     bg, ncr, ed, et = sess.run([self.model.bg_loss, self.model.ncr_loss, self.model.ed_loss, self.model.et_loss],
-                    #                                feed_dict=tr_feed_dict)
-                    #     s = bg + ncr + ed + et
-                    #     print('bg', ( bg/s ) * 100)
-                    #     print('ncr', ( ncr/s ) * 100)
-                    #     print('ed', ( ed/s ) * 100)
-                    #     print('et', ( et/s ) * 100)
-                    #
-                    #     total_cost += cost
-                    #     step += 1
-                    #
-                    #     # print out current epoch, step and batch loss value
-                    #     self.result = 'Cross validation : {0} / {1}, Epoch: {2} / {3}, Step: {4} / {5}, Batch loss: {6}'.format((idx + 1),
-                    #                                                                                                             cfg.SPLITS,
-                    #                                                                                                             epoch + 1,
-                    #                                                                                                             cfg.EPOCHS,
-                    #                                                                                                             step,
-                    #                                                                                                             train_step,
-                    #                                                                                                             cost)
-                    #
-                    #     print(self.result)
+                    for batch in tl.iterate.minibatches(inputs=train_X, targets=train_Y,
+                                                        batch_size=cfg.BATCH_SIZE, shuffle=True):
+                        batch_x, batch_y = batch
+
+                        # make_one_hot
+                        key = np.array(cfg.TRAIN_LABEL)
+                        # [0,1,2,3]
+                        _, index = np.unique(batch_y, return_inverse=True)
+                        # 4 -> 3
+                        seg = key[index].reshape(batch_y.shape)
+
+                        batch_y = np.eye(4)[seg]
+
+
+                        # step_time = time.time()
+                        tr_feed_dict = {self.model.X: batch_x,
+                                        self.model.Y: batch_y,
+                                        self.model.training: True,
+                                        self.model.drop_rate: 0.2}
+
+                        # cost, _ = sess.run([self.model.loss, self.optimizer], feed_dict=tr_feed_dict)
+                        cost, _ = sess.run([self.model.loss, self.optimizer], feed_dict=tr_feed_dict)
+
+                        bg, ncr, ed, et = sess.run([self.model.bg_loss, self.model.ncr_loss, self.model.ed_loss, self.model.et_loss],
+                                                   feed_dict=tr_feed_dict)
+                        s = bg + ncr + ed + et
+                        print('bg', ( bg/s ) * 100)
+                        print('ncr', ( ncr/s ) * 100)
+                        print('ed', ( ed/s ) * 100)
+                        print('et', ( et/s ) * 100)
+
+                        total_cost += cost
+                        step += 1
+
+                        # print out current epoch, step and batch loss value
+                        self.result = 'Cross validation : {0} / {1}, Epoch: {2} / {3}, Step: {4} / {5}, Batch loss: {6}'.format((idx + 1),
+                                                                                                                                cfg.SPLITS,
+                                                                                                                                epoch + 1,
+                                                                                                                                cfg.EPOCHS,
+                                                                                                                                step,
+                                                                                                                                train_step,
+                                                                                                                                cost)
+
+                        print(self.result)
                         # utils.result_saver(self.model_path + cfg.PATH_SLASH + self.result_txt, self.result)
 ###################################################
                     et_one_epoch_result_list = []
@@ -282,7 +282,7 @@ class Train:
                         #     print_img *= -1
                         #     print_img_idx = print_img_idx - 149
                         #################################################
-                        p = 0.0001
+                        p = 0.00005
                         for i in range(0, cfg.BATCH_SIZE, cfg.BATCH_SIZE//2):
                             # et -> ncr
                             ncr_mask = utils.masking_rgb(pred_print[1][i], color='blue')
@@ -320,7 +320,7 @@ class Train:
 
 
 
-                            result_image = cv2.addWeighted(ori, float(100 - 60) * p, et_tc_wt_mask, float(60) * p, 0) * 255
+                            result_image = cv2.addWeighted(ori, p, et_tc_wt_mask, 1, 0) * 255
 
                             cv2.imwrite('./img/epoch{}/result/batch{}_{}.jpg'.format(epoch+1, print_img_idx, i+1), result_image)
                             cv2.imwrite('./img/epoch{}/mask/batch{}_{}_ncr.jpg'.format(epoch+1, print_img_idx, i+1), ncr_mask)
