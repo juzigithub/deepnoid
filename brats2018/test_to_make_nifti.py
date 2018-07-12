808715bbfbee375c25a2643a8a4e7a67d0403690
pms <sunkid100000@gmail.com>
2018-07-12 20:23:19 +0900

    z

--- brats2018/test_to_make_nifti.py
+++ brats2018/test_to_make_nifti.py
@@ -99,7 +99,9 @@ class Test:
                     zero_padded = np.pad(img_list, ((3, 2), (30, 18), (41, 39)), 'constant')
 
                     # print(np.shape(zero_padded))
-                    zero_padded.transpose([2,1,0])
+                    zero_padded = np.transpose(zero_padded, [2,1,0])
+
+
                     print(np.shape(zero_padded))
                     np.save('./img/test/for_nifti/{}.npy'.format(self.patient_id_list[save_idx]), zero_padded)
                     img_list = []
