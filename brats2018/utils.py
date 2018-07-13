07f9ade004cadb19bf68be26337119a25b1f0879
pms <sunkid100000@gmail.com>
2018-07-13 13:39:48 +0900

    z

--- brats2018/utils.py
+++ brats2018/utils.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn.metrics import confusion_matrix
 from scipy.spatial.distance import directed_hausdorff
 import nibabel
-
+from tensorflow.python.ops import array_ops
 #############################################################################################################################
 #                                                    Layer Functions                                                        #
 #############################################################################################################################
@@ -288,14 +288,44 @@ def huber_loss(output, target, delta=1.0):
 #     return focal
 
 
-def focal_loss(output, target, epsilon=1e-6):
-    ndim = len(output.get_shape())
-    output /= tf.reduce_sum(output, axis=(ndim - 1), keep_dims=True)
-    output = tf.clip_by_value(output, epsilon, 1 - epsilon)
-    # focal = -tf.reduce_mean(tf.square(tf.ones_like(output)-output)*target*tf.log(output), axis=tuple(range(ndim-1)))
-    focal = -tf.reduce_sum(tf.square(tf.ones_like(output)-output)*target*tf.log(output), axis=tuple(range(ndim-1)))
+# def focal_loss(output, target, epsilon=1e-6):
+#     ndim = len(output.get_shape())
+#     output /= tf.reduce_sum(output, axis=(ndim - 1), keep_dims=True)
+#     output = tf.clip_by_value(output, epsilon, 1 - epsilon)
+#     # focal = -tf.reduce_mean(tf.square(tf.ones_like(output)-output)*target*tf.log(output), axis=tuple(range(ndim-1)))
+#     focal = -tf.reduce_sum(tf.square(tf.ones_like(output)-output)*target*tf.log(output), axis=tuple(range(ndim-1)))
+#
+#     return tf.reduce_sum(focal)
 
-    return tf.reduce_sum(focal)
+
+def focal_loss(prediction_tensor, target_tensor, alpha=0.25, gamma=2, epsilon=1e-6):
+    r"""Compute focal loss for predictions.
+        Multi-labels Focal loss formula:
+            FL = -alpha * (z-p)^gamma * log(p) -(1-alpha) * p^gamma * log(1-p)
+                 ,which alpha = 0.25, gamma = 2, p = sigmoid(x), z = target_tensor.
+    Args:
+     prediction_tensor: A float tensor of shape [batch_size, num_anchors,
+        num_classes] representing the predicted logits for each class
+     target_tensor: A float tensor of shape [batch_size, num_anchors,
+        num_classes] representing one-hot encoded classification targets
+     alpha: A scalar tensor for focal loss alpha hyper-parameter
+     gamma: A scalar tensor for focal loss gamma hyper-parameter
+    Returns:
+        loss: A (scalar) tensor representing the value of the loss function
+    """
+    sigmoid_p = tf.nn.sigmoid(prediction_tensor)
+    zeros = array_ops.zeros_like(sigmoid_p, dtype=sigmoid_p.dtype)
+
+    # For poitive prediction, only need consider front part loss, back part is 0;
+    # target_tensor > zeros <=> z=1, so poitive coefficient = z - p.
+    pos_p_sub = array_ops.where(target_tensor > zeros, target_tensor - sigmoid_p, zeros)
+
+    # For negative prediction, only need consider back part loss, front part is 0;
+    # target_tensor > zeros <=> z=1, so negative coefficient = 0.
+    neg_p_sub = array_ops.where(target_tensor > zeros, zeros, sigmoid_p)
+    per_entry_cross_ent = - alpha * (pos_p_sub ** gamma) * tf.log(tf.clip_by_value(sigmoid_p, 1e-8, 1.0)) \
+                          - (1 - alpha) * (neg_p_sub ** gamma) * tf.log(tf.clip_by_value(1.0 - sigmoid_p, 1e-8, 1.0))
+    return tf.reduce_sum(per_entry_cross_ent)
 
 
 '''
