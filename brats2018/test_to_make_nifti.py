51f63fe794da77cb9fa0e15eb55cd8797856d7f7
pms <sunkid100000@gmail.com>
2018-07-14 17:21:06 +0900

    z

--- brats2018/test_to_make_nifti.py
+++ brats2018/test_to_make_nifti.py
@@ -96,7 +96,7 @@ class Test:
                     zero_padded = np.flip(zero_padded,1)
 
                     # utils.save_array_as_nifty_volume(zero_padded, './img/test/for_nifti/{}.nii.gz'.format(self.patient_id_list[save_idx]))
-                    np.save(zero_padded, './img/test/for_nifti/{}.npy'.format(self.patient_id_list[save_idx]))
+                    np.save('./img/test/for_nifti/{}.npy'.format(self.patient_id_list[save_idx]), zero_padded)
                     img_list = []
                     img_idx = 0
                     save_idx += 1
