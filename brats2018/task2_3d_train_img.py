3a2a4b30b2c33cf89dbdab4533007c2596dce598
pms <sunkid100000@gmail.com>
2018-07-14 20:06:23 +0900

    z

--- brats2018/task2_3d_train_img.py
+++ brats2018/task2_3d_train_img.py
@@ -28,7 +28,7 @@ _, length, _, _ = np.shape(label)
 length //= 150
 for idx in range(length):
     survival_img = label[:,150*idx:150*(idx+1),:,:].transpose([0, 3, 2, 1])
-    survival_img = np.pad(survival_img, ((0, 0), (41, 39), (30, 18), (3, 2)), 'constant')
+    # survival_img = np.pad(survival_img, ((0, 0), (41, 39), (30, 18), (3, 2)), 'constant')
     # survival_img = np.flip(survival_img, axis=1)
     # survival_img = np.flip(survival_img, axis=2)
     print(np.shape(survival_img))
