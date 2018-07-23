4c965cf811e68ead14a8c3c35c8324c4aa8a7c64
pms <sunkid100000@gmail.com>
2018-07-23 10:14:17 +0900

    z

--- brats2018/loader.py
+++ brats2018/loader.py
@@ -106,8 +106,8 @@ def get_normalized_img(data_sets, train, task1=True):
         if idx < cfg.N_INPUT_CHANNEL:
             shape = np.shape(imgset)
             imgset = imgset.reshape([len(imgset), -1])
-            minmax_scale(imgset, axis=1, copy=False)
-            # scale(imgset, axis=1, copy=False)
+            # minmax_scale(imgset, axis=1, copy=False)
+            scale(imgset, axis=1, copy=False)
             # imgset = imgset / (np.max(imgset, axis=1) + 1e-6).reshape([-1,1])
             imgset = imgset.reshape(shape)
             total_norm_list[idx] = imgset
