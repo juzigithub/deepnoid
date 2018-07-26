4cd1c8e04433bba9f54f2d9a610c5e776ef80db4
pms <sunkid100000@gmail.com>
2018-07-26 15:14:41 +0900

    z

--- brats2018/nifti_checker.py
+++ brats2018/nifti_checker.py
@@ -27,38 +27,38 @@ def masking_rgb(img, color=None):
     return out_img
 
 
-original_path = 'd:\\Brats18_CBICA_AAM_1_flair.nii.gz'
+# original_path = 'd:\\Brats18_CBICA_AAM_1_flair.nii.gz'
 result_path = 'd:\\Brats18_CBICA_AAM_1.nii.gz'
 
-original_img = nb.load(original_path).get_fdata().transpose((-1,1,0))
-original_img /= np.max(original_img)
+# original_img = nb.load(original_path).get_fdata().transpose((-1,1,0))
+# original_img /= np.max(original_img)
 result_img = nb.load(result_path).get_fdata().transpose((-1,1,0))
 
-print(original_img.shape)
+# print(original_img.shape)
 print(result_img.shape)
 
 for i in range(240):
     for j in range(240):
-        print(result_img[80][i][j])
+        print(result_img[6][i][j])
 
-for i in range(155):
-    ncr_mask = masking_rgb(result_img[i], color='green')
-    ncr_mask[ncr_mask!=255] = 0
-    ed_mask = masking_rgb(result_img[i], color='blue')
-    ed_mask[ed_mask!=255*2] = 0
-    et_mask = masking_rgb(result_img[i], color='red')
-    et_mask[et_mask!=255*4] = 0
-
-    et_tc_wt = ncr_mask + ed_mask + et_mask
-    shape = et_tc_wt.shape
-    et_tc_wt_mask = et_tc_wt.reshape((-1,3))
-    len_mask = len(et_tc_wt_mask)
-    et_tc_wt_mask -= (0.9 * et_tc_wt_mask.max(1).reshape([len_mask, -1]) - et_tc_wt_mask.min(1).reshape([len_mask, -1]))
-    et_tc_wt_mask = np.clip(et_tc_wt_mask, 0., 1.) * 255
-    et_tc_wt_mask = et_tc_wt_mask.reshape(shape)
-
-    ori = masking_rgb(original_img[i])
-    result_image = 0.7 * (ori + et_tc_wt_mask)
-    print(result_image.shape)
-    cv2.imwrite('d:\\img\\result_{}.jpg'.format(i + 1), result_image)
-    cv2.imwrite('d:\\img\\original_{}.jpg'.format(i + 1), ori)
+# for i in range(155):
+#     ncr_mask = masking_rgb(result_img[i], color='green')
+#     ncr_mask[ncr_mask!=255] = 0
+#     ed_mask = masking_rgb(result_img[i], color='blue')
+#     ed_mask[ed_mask!=255*2] = 0
+#     et_mask = masking_rgb(result_img[i], color='red')
+#     et_mask[et_mask!=255*4] = 0
+#
+#     et_tc_wt = ncr_mask + ed_mask + et_mask
+#     shape = et_tc_wt.shape
+#     et_tc_wt_mask = et_tc_wt.reshape((-1,3))
+#     len_mask = len(et_tc_wt_mask)
+#     et_tc_wt_mask -= (0.9 * et_tc_wt_mask.max(1).reshape([len_mask, -1]) - et_tc_wt_mask.min(1).reshape([len_mask, -1]))
+#     et_tc_wt_mask = np.clip(et_tc_wt_mask, 0., 1.) * 255
+#     et_tc_wt_mask = et_tc_wt_mask.reshape(shape)
+#
+#     ori = masking_rgb(original_img[i])
+#     result_image = 0.7 * (ori + et_tc_wt_mask)
+#     print(result_image.shape)
+#     cv2.imwrite('d:\\img\\result_{}.jpg'.format(i + 1), result_image)
+#     cv2.imwrite('d:\\img\\original_{}.jpg'.format(i + 1), ori)
--- brats2018/utils.py
+++ brats2018/utils.py
@@ -1259,20 +1259,31 @@ def extract_patches_from_batch(imgs, patch_shape, stride):
 def reconstruct_from_patches_nd(patches, image_shape, stride):
     # modified version of sklearn.feature_extraction.image.reconstruct_from_patches_2d
     # It can make only one image
-    i_h, i_w = image_shape[:2]
-    p_h, p_w = patches.shape[1:3]
-    img = np.zeros(image_shape)
-    img_overlapped = np.zeros(image_shape)
+    if patches.ndim == 3:
+        i_h, i_w = image_shape[:2]
+        p_h, p_w = patches.shape[1:3]
+        img = np.zeros(image_shape)
+        img_overlapped = np.zeros(image_shape)
 
-    n_h = i_h - p_h + 1
-    n_w = i_w - p_w + 1
-    for p, (i, j) in zip(patches, product(range(0,n_h,stride), range(0,n_w,stride))):
-        if patches.ndim == 3:
+        n_h = i_h - p_h + 1
+        n_w = i_w - p_w + 1
+        for p, (i, j) in zip(patches, product(range(0,n_h,stride), range(0,n_w,stride))):
             img[i:i + p_h, j:j + p_w] += p
             img_overlapped[i:i + p_h, j:j + p_w] += 1
-        elif patches.ndim == 4:
-            img[i:i + p_h, j:j + p_w,:] += p
-            img_overlapped[i:i + p_h, j:j + p_w,:] += 1
+
+    elif patches.ndim == 4:
+        i_h, i_w, i_c = image_shape[:3]
+        p_h, p_w, p_c = patches.shape[1:4]
+        img = np.zeros(image_shape)
+        img_overlapped = np.zeros(image_shape)
+
+        n_h = i_h - p_h + 1
+        n_w = i_w - p_w + 1
+        n_c = i_c - p_c + 1
+        for p, (i, j, k) in zip(patches, product(range(0, n_h, stride), range(0, n_w, stride), range(0, n_c, stride))):
+            img[i:i + p_h, j:j + p_w, k:k + p_c] += p
+            img_overlapped[i:i + p_h, j:j + p_w, k:k + p_c] += 1
+
     img /= img_overlapped
 
     return img
