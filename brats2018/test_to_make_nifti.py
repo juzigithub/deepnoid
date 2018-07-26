15529a3eba16e154825763fad206beeb9290d1d5
pms <sunkid100000@gmail.com>
2018-07-26 10:28:25 +0900

    z

--- brats2018/test_to_make_nifti.py
+++ brats2018/test_to_make_nifti.py
@@ -91,7 +91,7 @@ class Test:
                 pred.astype(np.float32)
                 print(np.shape(pred))
                 patch_list.append(pred.tolist())
-                # patch_list = np.array(patch_list).reshape([-1,192,160])
+                patch_list = np.array(patch_list)
                 patch_list = utils.reconstruct_from_patches_nd(patch_list, (cfg.IMG_SIZE[0], cfg.IMG_SIZE[1]), cfg.PATCH_STRIDE)
                 img_list.append(patch_list.tolist())
                 patch_list = []
