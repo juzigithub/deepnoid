043136431267b5e5e1b7060effc9be42d156afb3
pms <sunkid100000@gmail.com>
2018-08-07 09:36:42 +0900

    z

--- brats2018/train3.py
+++ brats2018/train3.py
@@ -133,48 +133,48 @@ class Train:
                     self._make_path(epoch)
                 #
                 # # train
-                for batch in tl.iterate.minibatches(inputs=train_X, targets=train_Y,
-                                                    batch_size=cfg.BATCH_SIZE, shuffle=True):
-                    batch_x, batch_y = batch
-
-                    # make_one_hot
-                    key = np.array(cfg.TRAIN_LABEL)
-                    _, index = np.unique(batch_y, return_inverse=True)
-                    seg = key[index].reshape(batch_y.shape)
-                    batch_y = np.eye(4)[seg]
-
-                    tr_feed_dict = {self.model.X: batch_x,
-                                    self.model.Y: batch_y,
-                                    self.model.training: True,
-                                    self.model.drop_rate: drop_rate,
-                                    self.model.loss_ratio: loss_ratio}
-
-                    cost, _ = sess.run([self.model.loss, self.optimizer], feed_dict=tr_feed_dict)
-
-                    bg, ncr, ed, et = sess.run([self.model.bg_loss, self.model.ncr_loss, self.model.ed_loss, self.model.et_loss],
-                                               feed_dict=tr_feed_dict)
-
-                    s = bg + ncr + ed + et
-                    print('bg loss ratio : ', (bg/s) * 100)
-                    print('ncr loss ratio : ', ( ncr/s ) * 100)
-                    print('ed loss ratio : ', ( ed/s ) * 100)
-                    print('et loss ratio : ', ( et/s ) * 100)
-
-                    # Update Loss Ratio for next step
-                    loss_ratio = loss_ratio * np.sqrt([bg, ncr, ed, et])
-                    loss_ratio = loss_ratio / np.sum(loss_ratio)
-
-                    total_cost += cost
-                    step += 1
-
-                    # print out current epoch, step and batch loss value
-                    self.result = 'Epoch: {0} / {1}, Step: {2} / {3}, Batch loss: {4}'.format(epoch + 1,
-                                                                                              cfg.EPOCHS,
-                                                                                              step,
-                                                                                              train_step,
-                                                                                              cost)
-
-                    print(self.result)
+                # for batch in tl.iterate.minibatches(inputs=train_X, targets=train_Y,
+                #                                     batch_size=cfg.BATCH_SIZE, shuffle=True):
+                #     batch_x, batch_y = batch
+                #
+                #     # make_one_hot
+                #     key = np.array(cfg.TRAIN_LABEL)
+                #     _, index = np.unique(batch_y, return_inverse=True)
+                #     seg = key[index].reshape(batch_y.shape)
+                #     batch_y = np.eye(4)[seg]
+                #
+                #     tr_feed_dict = {self.model.X: batch_x,
+                #                     self.model.Y: batch_y,
+                #                     self.model.training: True,
+                #                     self.model.drop_rate: drop_rate,
+                #                     self.model.loss_ratio: loss_ratio}
+                #
+                #     cost, _ = sess.run([self.model.loss, self.optimizer], feed_dict=tr_feed_dict)
+                #
+                #     bg, ncr, ed, et = sess.run([self.model.bg_loss, self.model.ncr_loss, self.model.ed_loss, self.model.et_loss],
+                #                                feed_dict=tr_feed_dict)
+                #
+                #     s = bg + ncr + ed + et
+                #     print('bg loss ratio : ', (bg/s) * 100)
+                #     print('ncr loss ratio : ', ( ncr/s ) * 100)
+                #     print('ed loss ratio : ', ( ed/s ) * 100)
+                #     print('et loss ratio : ', ( et/s ) * 100)
+                #
+                #     # Update Loss Ratio for next step
+                #     loss_ratio = loss_ratio * np.sqrt([bg, ncr, ed, et])
+                #     loss_ratio = loss_ratio / np.sum(loss_ratio)
+                #
+                #     total_cost += cost
+                #     step += 1
+                #
+                #     # print out current epoch, step and batch loss value
+                #     self.result = 'Epoch: {0} / {1}, Step: {2} / {3}, Batch loss: {4}'.format(epoch + 1,
+                #                                                                               cfg.EPOCHS,
+                #                                                                               step,
+                #                                                                               train_step,
+                #                                                                               cost)
+                #
+                #     print(self.result)
 
                 et_one_epoch_result_list = []
                 tc_one_epoch_result_list = []
