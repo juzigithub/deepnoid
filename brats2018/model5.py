5b372a143b70bf946fe0395ddf9538aab34c6105
pms <sunkid100000@gmail.com>
2018-08-08 19:48:24 +0900

    z

--- brats2018/model5.py
+++ brats2018/model5.py
@@ -83,7 +83,7 @@ class Model:
             self.down_conv[-1] = utils.atrous_spatial_pyramid_pooling(name='aspp_layer',
                                                                       inputs=inputs,
                                                                       channel_n=channel_n,
-                                                                      atrous_rate_list=[8,12,16],
+                                                                      atrous_rate_list=[[8,8],[12,12],[16,16]],
                                                                       act_fn=cfg.ACTIVATION_FUNC,
                                                                       training=self.training)
         with tf.variable_scope('up'):
