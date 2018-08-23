1657e961bc00eb58e8de6099991ec642799a2c92
pms <sunkid100000@gmail.com>
2018-08-23 10:48:55 +0900

    z

--- aneurysm_yonsei/loader.py
+++ aneurysm_yonsei/loader.py
@@ -41,16 +41,17 @@ def save_resized_dcm_as_npy(data_path, save_path, filename):
 
 
         x_img = utils.extract_patches_from_batch(x_img, (cfg.PATCH_SIZE, cfg.PATCH_SIZE), cfg.PATCH_STRIDE)
-        y_img = utils.extract_patches_from_batch(y_img, (cfg.PATCH_SIZE, cfg.PATCH_SIZE), cfg.PATCH_STRIDE)
 
 
         y_img_fg = cv2.threshold(y_img, 50, 1, cv2.THRESH_BINARY)[1]
-        y_img_fg = y_img_fg.reshape((-1, cfg.PATCH_SIZE, cfg.PATCH_SIZE, 1))
+        y_img_fg = y_img_fg.reshape((1, cfg.PATCH_SIZE, cfg.PATCH_SIZE, 1))
         y_img_bg = cv2.threshold(y_img, 50, 1, cv2.THRESH_BINARY_INV)[1]
-        y_img_bg = y_img_bg.reshape((-1, cfg.PATCH_SIZE, cfg.PATCH_SIZE, 1))
+        y_img_bg = y_img_bg.reshape((1, cfg.PATCH_SIZE, cfg.PATCH_SIZE, 1))
 
         y_img = np.concatenate((y_img_bg, y_img_fg), axis=-1)
         y_img = np.argmax(y_img, axis=-1)
+
+        y_img = utils.extract_patches_from_batch(y_img, (cfg.PATCH_SIZE, cfg.PATCH_SIZE), cfg.PATCH_STRIDE)
 
         x_y_img = np.concatenate((x_img, y_img), axis=0)
 
