448855e5560d2c3d62b62a91d15fd8e83bee09ea
pms <sunkid100000@gmail.com>
2018-08-27 12:06:57 +0900

    z

--- aneurysm_yonsei/test.py
+++ aneurysm_yonsei/test.py
@@ -91,7 +91,10 @@ class Test:
                 ori_patch_list = utils.reconstruct_from_patches_nd(batch_x, (cfg.IMG_SIZE[0], cfg.IMG_SIZE[1], 1), cfg.PATCH_STRIDE)
 
                 pred_patch_list = np.argmax(pred_patch_list, axis=-1)
-
+                for i in range(256):
+                    for j in range(256):
+                        if pred_patch_list[i][j] != 0:
+                            print(pred_patch_list[i][j])
                 num_labels, markers, states, cent = cv2.connectedComponentsWithStats(np.uint8(pred_patch_list))
                 print('states', states)
                 for state in states:
