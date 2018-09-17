f3fc6e927cac447ea7f8e8ae28c525a82e7cd10a
pms <sunkid100000@gmail.com>
2018-09-17 11:36:31 +0900

    z

--- aneurysm_detection/feature_extractor_pretrain/loader.py
+++ aneurysm_detection/feature_extractor_pretrain/loader.py
@@ -28,12 +28,20 @@ for data in data_list:
 
 len_npy_list = np.shape(npy_list)[0]
 len_chunk = len_npy_list // 5
+#
+# np.savez_compressed(os.path.join(cfg.NPZ_PATH, 'feature_extractor_pretrain_input_ori_{}_{}.npz'.format(cfg.IMG_SIZE[0], 0)), chunk0=npy_list[len_chunk*0:len_chunk*1])
+# np.savez_compressed(os.path.join(cfg.NPZ_PATH, 'feature_extractor_pretrain_input_ori_{}_{}.npz'.format(cfg.IMG_SIZE[0], 1)), chunk1=npy_list[len_chunk*1:len_chunk*2])
+# np.savez_compressed(os.path.join(cfg.NPZ_PATH, 'feature_extractor_pretrain_input_ori_{}_{}.npz'.format(cfg.IMG_SIZE[0], 2)), chunk2=npy_list[len_chunk*2:len_chunk*3])
+# np.savez_compressed(os.path.join(cfg.NPZ_PATH, 'feature_extractor_pretrain_input_ori_{}_{}.npz'.format(cfg.IMG_SIZE[0], 3)), chunk3=npy_list[len_chunk*3:len_chunk*4])
+# np.savez_compressed(os.path.join(cfg.NPZ_PATH, 'feature_extractor_pretrain_input_ori_{}_{}.npz'.format(cfg.IMG_SIZE[0], 4)), chunk4=npy_list[len_chunk*4: ])
+#
 
-np.savez_compressed(os.path.join(cfg.NPZ_PATH, 'feature_extractor_pretrain_input_ori_{}_{}.npz'.format(cfg.IMG_SIZE[0], 0)), chunk0=npy_list[len_chunk*0:len_chunk*1])
-np.savez_compressed(os.path.join(cfg.NPZ_PATH, 'feature_extractor_pretrain_input_ori_{}_{}.npz'.format(cfg.IMG_SIZE[0], 1)), chunk1=npy_list[len_chunk*1:len_chunk*2])
-np.savez_compressed(os.path.join(cfg.NPZ_PATH, 'feature_extractor_pretrain_input_ori_{}_{}.npz'.format(cfg.IMG_SIZE[0], 2)), chunk2=npy_list[len_chunk*2:len_chunk*3])
-np.savez_compressed(os.path.join(cfg.NPZ_PATH, 'feature_extractor_pretrain_input_ori_{}_{}.npz'.format(cfg.IMG_SIZE[0], 3)), chunk3=npy_list[len_chunk*3:len_chunk*4])
-np.savez_compressed(os.path.join(cfg.NPZ_PATH, 'feature_extractor_pretrain_input_ori_{}_{}.npz'.format(cfg.IMG_SIZE[0], 4)), chunk4=npy_list[len_chunk*4: ])
+np.save(os.path.join(cfg.NPZ_PATH, 'feature_extractor_pretrain_input_ori_{}_{}.npy'.format(cfg.IMG_SIZE[0], 0)), npy_list[len_chunk*0:len_chunk*1])
+np.save(os.path.join(cfg.NPZ_PATH, 'feature_extractor_pretrain_input_ori_{}_{}.npy'.format(cfg.IMG_SIZE[0], 1)), npy_list[len_chunk*1:len_chunk*2])
+np.save(os.path.join(cfg.NPZ_PATH, 'feature_extractor_pretrain_input_ori_{}_{}.npy'.format(cfg.IMG_SIZE[0], 2)), npy_list[len_chunk*2:len_chunk*3])
+np.save(os.path.join(cfg.NPZ_PATH, 'feature_extractor_pretrain_input_ori_{}_{}.npy'.format(cfg.IMG_SIZE[0], 3)), npy_list[len_chunk*3:len_chunk*4])
+np.save(os.path.join(cfg.NPZ_PATH, 'feature_extractor_pretrain_input_ori_{}_{}.npy'.format(cfg.IMG_SIZE[0], 4)), npy_list[len_chunk*4: ])
+
 
 print('saved')
 
