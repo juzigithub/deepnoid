f2dbce98c40c7f920f9d1f11e4cc587099753471
pms <sunkid100000@gmail.com>
2018-09-17 11:20:45 +0900

    z

--- aneurysm_detection/feature_extractor_pretrain/loader.py
+++ aneurysm_detection/feature_extractor_pretrain/loader.py
@@ -26,7 +26,14 @@ for data in data_list:
     img = (img - np.mean(img)) / np.max(img)
     npy_list.append(img)
 
-print(np.shape(npy_list))
-np.savez_compressed(os.path.join(cfg.NPZ_PATH, 'feature_extractor_pretrain_input_ori_{}.npz'.format(cfg.IMG_SIZE[0])), all=npy_list)
+len_npy_list = np.shape(npy_list)[0]
+len_chunk = len_npy_list // 5
+
+np.savez_compressed(os.path.join(cfg.NPZ_PATH, 'feature_extractor_pretrain_input_ori_{}_{}.npz'.format(cfg.IMG_SIZE[0], 0)), chunk0=npy_list[len_chunk*0:len_chunk*1])
+np.savez_compressed(os.path.join(cfg.NPZ_PATH, 'feature_extractor_pretrain_input_ori_{}_{}.npz'.format(cfg.IMG_SIZE[0], 1)), chunk1=npy_list[len_chunk*1:len_chunk*2])
+np.savez_compressed(os.path.join(cfg.NPZ_PATH, 'feature_extractor_pretrain_input_ori_{}_{}.npz'.format(cfg.IMG_SIZE[0], 2)), chunk2=npy_list[len_chunk*2:len_chunk*3])
+np.savez_compressed(os.path.join(cfg.NPZ_PATH, 'feature_extractor_pretrain_input_ori_{}_{}.npz'.format(cfg.IMG_SIZE[0], 3)), chunk3=npy_list[len_chunk*3:len_chunk*4])
+np.savez_compressed(os.path.join(cfg.NPZ_PATH, 'feature_extractor_pretrain_input_ori_{}_{}.npz'.format(cfg.IMG_SIZE[0], 4)), chunk4=npy_list[len_chunk*4: ])
+
 print('saved')
 
